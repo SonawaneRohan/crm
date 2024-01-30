53b6d6e33b45725335b18cc178a9a40063fe3b57
SonawaneRohan <rohansonawane123@gmail.com>
2024-01-30 23:42:48 +0530

    Initial commit

---
+++ website/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authentication 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "loged in...")
+            return redirect('home')
+        else:
+            messages.success(request, "there is an error to log in")
+            return redirect('home')
+    else:
+      return render(request, 'home.html', {})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+        #  form.save()
+        # Authenticate and login
+          username = form.cleaned_data['username']
+          password = form.cleaned_data['password1']
+          user = authenticate(username=username, password=password)
+          login(request, user)
+          messages.success(request, "You've Successfully register")
+        return redirect('home')
+    else:
+        form = SignUpForm()
+
+        return render(request, 'register.html', )
+
+
+
+
+
+
+
+
